81ea68eaca4a99b39378735bf7a7f5ad93cd5c48
love1234 <345841634@qq.com>
2018-09-20 14:01:20 +0800

    开启antd mobile样式的按需加载

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,21 +1,18 @@
 const path = require('path')
 export default {
-  plugins:[
-    ["import", { libraryName: "antd-mobile", style: "less" }] // `style: true` 会加载 less 文件
-  ],
-    alias:{
-        components:path.resolve(__dirname,'src/components'),
-        utils:path.resolve(__dirname,'src/utils'),
-        services:path.resolve(__dirname,'src/services'),
-        models:path.resolve(__dirname,'src/models'),
-        themes:path.resolve(__dirname,'src/themes'),
-        images:path.resolve(__dirname,'src/assets')
-    },
-    // proxy: {
-    //     "/api/v1/th": {
-    //       "target": "http://url",
-    //       "changeOrigin": true,
-    //       "pathRewrite": { "^/api/v1/th": "" }
-    //     },
-    //   },
+  alias: {
+    components: path.resolve(__dirname, 'src/components'),
+    utils: path.resolve(__dirname, 'src/utils'),
+    services: path.resolve(__dirname, 'src/services'),
+    models: path.resolve(__dirname, 'src/models'),
+    themes: path.resolve(__dirname, 'src/themes'),
+    images: path.resolve(__dirname, 'src/assets')
+  },
+  // proxy: {
+  //     "/api/v1/th": {
+  //       "target": "http://url",
+  //       "changeOrigin": true,
+  //       "pathRewrite": { "^/api/v1/th": "" }
+  //     },
+  //   },
 }
