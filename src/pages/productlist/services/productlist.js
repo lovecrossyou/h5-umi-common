a5dcc387b8abe98a3d51bfa1f7633b7ca8d3781a
love1234 <345841634@qq.com>
2018-09-27 12:58:07 +0800

    xx

--- src/pages/productlist/services/productlist.js
+++ src/pages/productlist/services/productlist.js
@@ -2,7 +2,7 @@ import request from '../../../utils/request';
 
 export async function queryProductList(param) {
   return request({
-    url:'/api/service/merchant/integralProductList',
+    url:'/service/merchant/integralProductList',
     method: 'post',
     body:param
   });
